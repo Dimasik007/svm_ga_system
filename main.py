41f10383062702b139284de3d788c5756e6b216c
Dmitriy <dmitriy00vn@gmail.com>
2018-08-20 12:11:28 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 28 13:57:52 2018
+
+@author: Dimasik007
+"""
+# path to the data files
+# /Users/Dimasik007/Desktop/MSc_project/data/usdt_eth_2h.csv
+# 1420070400 unix timestamp for 1st Jan 2015
+# 1451606400 unix timestamp for 1st Jan 2016
+
+
+import pandas as pd  # data analysis and manipulation
+import numpy as np
+import matplotlib.pyplot as plt  # visualisation library
+import seaborn as sns  # library for visualisation of statistical data
+from datetime import datetime
+from matplotlib import style
+from collections import Counter
+from sklearn import svm, cross_validation, neighbors  # cross_validation depreciated in favor of model_selection
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+# %matplotlib inline  # execute this in IPython console to put figuers inline
+pd.set_option('display.notebook_repr_html', False)
+pd.set_option('display.max_columns', 8)
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.width', 78)
+pd.set_option('precision', 6)
+sns.set()
+# style.use('ggplot')
+
+path = '/Users/Dimasik007/Desktop/Programming/PyCharm/Dissertation/svm_ga_system/data/'
+
+# to read csv file and save it to a variable; set date as index
+us_eth2h = pd.read_csv('/Users/Dimasik007/Desktop/MSc_project/data/usdt_eth_2h.csv',
+                       index_col=['date'], parse_dates=True,
+                       infer_datetime_format=True, dayfirst=True)
+
+
+tickersUSD = ['USDT_BTC', 'USDT_BCH', 'USDT_ETC', 'USDT_XMR',
+           'USDT_ETH', 'USDT_DASH', 'USDT_XRP', 'USDT_LTC',
+           'USDT_NXT', 'USDT_STR', 'USDT_REP', 'USDT_ZEC']
+
+
+# check pythonprogramming.net to make it more automated
+def get_crypto_data(symbol, frequency, start=0, tocsv=False):
+    '''
+    Params: String symbol, int frequency in s = 300,900,1800,7200,14400,86400
+    Returns: time series of ticker in a dataframe
+    If tocsv=True creates a csv file, else returns df to assign to variable
+    change needed fileName.csv to account for frequency in conditional
+    '''
+    url = 'https://poloniex.com/public?command=returnChartData&currencyPair=' \
+          + symbol + '&end=9999999999&period=' + str(frequency) + '&start=' + str(start)
+    df = pd.read_json(url)
+    df.set_index('date', inplace=True)
+    if tocsv:
+        df.to_csv(symbol + '_daily.csv')  # add granularity before '.csv'
+    else:
+        return df
+    print('Processed: ' + symbol)
+
+
+# to download all tickrs and save to scv
+for ticker in tickersUSD:
+    get_crypto_data(ticker, frequency=86400, start=0, tocsv=True)
+
+
+# combine all csv into one df with closing prices, fill NaN with 0
+crypto_df = pd.DataFrame()
+for ticker in tickersUSD:
+    crypto_df[ticker] = pd.read_csv(path+ticker+'_daily.csv', index_col='date', parse_dates=True)['close']
+
+crypto_df.fillna(0, inplace=True)  # fill NaN with 0
+
+
+# assign ticker to a variable df
+usdt_eth2h = get_crypto_data('USDT_ETH', 7200, 1451606400, tocsv=False)
+
+'''
+# building correlation table
+crypto_corr = crypto_df['2017-06':'2018-06'].corr()
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(14, 10))
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(crypto_corr, xticklabels=crypto_corr.columns.values,
+            yticklabels=crypto_corr.columns.values,
+            square=True, linewidths=.5)
+
+# visualise correlations between columns
+sns.heatmap(crypto_corr,
+            xticklabels=crypto_corr.columns.values,
+            yticklabels=crypto_corr.columns.values)
+'''
+
+
+def process_data_for_labels(ticker):
+    hm_days = 7
+    # df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
+    df = crypto_df
+    tickers = tickersUSD
+    df.fillna(0, inplace=True)
+
+    for i in range(1, hm_days+1):
+        df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
+
+    df.fillna(0, inplace=True)
+    return tickers, df
+
+
+def buy_sell_hold(*args):  # *args allows to pass any number of parameters and iterable
+    cols = [c for c in args]
+    requirement = 0.02  # required percent change
+    for col in cols:
+        if col > requirement:
+            return 1  # buy
+        if col < -requirement:
+            return -1  # sell
+    return 0  # hold / do nothing
+
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map(buy_sell_hold, df['{}_1d'.format(ticker)],
+                                                             df['{}_2d'.format(ticker)],
+                                                             df['{}_3d'.format(ticker)],
+                                                             df['{}_4d'.format(ticker)],
+                                                             df['{}_5d'.format(ticker)],
+                                                             df['{}_6d'.format(ticker)],
+                                                             df['{}_7d'.format(ticker)]
+                                                ))
+
+    vals = df['{}_target'.format('USDT_BTC')].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    # replace infinite data with nan (eg after pct_change with 0)
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return X, y, df
+
+
+def do_ml(ticker):
+    '''
+
+    '''
+    X, y, df = extract_featuresets(ticker)
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,
+                                                                         y,
+                                                                         test_size=0.25)
+    # defining classifiers used in voting classifier
+    clf = VotingClassifier([('lsvc', svm.LinearSVC()),
+                            ('knn', neighbors.KNeighborsClassifier()),
+                            ('rfor', RandomForestClassifier())])
+    # fit input (X_train: pct_change) to target (Y_train: 1, 0, -1)
+    clf.fit(X_train, y_train)  # train classifier
+    confidence = clf.score(X_test, y_test)  # get accuracy score
+    print('accuracy:', confidence)
+    predictions = clf.predict(X_test)
+    print('predicted class counts:', Counter(predictions))
+    print()
+    print()
+    return confidence
+
+
+do_ml('USDT_BCH')
+
+# to save trained model if it works well
+# with open('svc_model_prices.pickle', 'wb') as f:
+#     pickle.dump(clf, f)
+# pickle_in = open('svc_model_prices.pickle', 'rb')
+# clf_from_pickle = pickle.load(pickle_in)
+
+
+# calculate MAs on weighted average column and add them as columns to dataframe
+us_eth2h['MA7'] = us_eth2h['weightedAverage'].rolling(window=7, min_periods=1).mean()
+us_eth2h['MA30'] = us_eth2h['weightedAverage'].rolling(window=30, min_periods=1).mean()
+us_eth2h['MA90'] = us_eth2h['weightedAverage'].rolling(window=90, min_periods=1).mean()
+us_eth2h['MA120'] = us_eth2h['weightedAverage'].rolling(window=120, min_periods=1).mean()
+
+# to plot MAs and weighted average price
+us_eth2h['2017-01':'2018-06'][['weightedAverage', 'MA7', 'MA30', 'MA120']].plot(figsize=(14, 10))
+
+
+"""
+# plotting everything together___________________________________________________________________________________
+# Prepare plot (4 subplots on 1 column)
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True)
+# Remove horizontal space between axes
+fig.subplots_adjust(hspace=0)
+fig.xlabel('Year', fontsize=25)
+
+# set plot size
+fig.set_size_inches(15, 30)
+
+# plotting closing prices
+ax1.set_ylabel('EUR / USD', size=25)
+EUR_USD.Close.resample('M').mean().plot(ax=ax1, c='black')
+# plotting moving averages
+EUR_USD.sma.resample('M').mean().plot(ax=ax1, c='r', label='SMA30')
+EUR_USD.ema.resample('M').mean().plot(ax=ax1, c='g', label='EMA30')
+EUR_USD.wma.resample('M').mean().plot(ax=ax1, c='b', label='WMA30')
+# setting legend
+handles, labels = ax1.get_legend_handles_labels()
+ax1.legend(handles, labels)
+
+# plotting RSI subplot
+ax2.set_ylabel('RSI', size=20)
+EUR_USD.rsi.resample('M').mean().plot(ax=ax2, c='g', label='RSI 14')
+ax2.axhline(y=30, c='b')
+ax2.axhline(y=50, c='black')
+ax2.axhline(y=70, c='b')
+ax2.set_ylim([0, 100])
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend(handles, labels)
+
+# plotting MACD
+ax3.set_ylabel('MACD: 12, 26, 9', size=20)
+EUR_USD.macd.resample('M').mean().plot(ax=ax3, color='b', label='Macd')
+EUR_USD.macdSignal.resample('M').mean().plot(ax=ax3, color='g', label='Signal')
+EUR_USD.macdHist.resample('M').mean().plot(ax=ax3, color='r', label='Hist')
+ax3.axhline(0, lw=2, color='0')
+handles, labels = ax3.get_legend_handles_labels()
+ax3.legend(handles, labels)
+
+# plotting ROC
+ax4.set_ylabel('ROC: 10', size=20)
+EUR_USD.roc.resample('M').mean().plot(ax=ax4, color='r', label='ROC')
+handles, labels = ax4.get_legend_handles_labels()
+ax4.legend(handles, labels)
+
+# plt.show()_____________________________________________________________________________________________________
+"""
---
+++ svm_ga.py
@@ -0,0 +1,369 @@
+import warnings
+warnings.filterwarnings('always')  # "error", "ignore", "always", "default", "module" or "once"
+import pandas as pd
+import numpy as np
+import talib as ta
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, KFold
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+from sklearn.preprocessing import StandardScaler, label_binarize
+from sklearn.utils import resample, class_weight
+from sklearn.svm import SVC
+import pickle
+sns.set()
+
+'''
+to do list:
+FX data layer:
+1) load csv EUR/USD
+2) calculate Technical Indicators ( RSI, ROC, SMA, EMA, WMA, MACD )
+3) implement voting system based on TI
+
+Optimisation Layer
+    SVM
+* train SVM on past price data and on TI values and compare
+1) implement SVM which classifies market state (1, 0, -1)
+2) implement grid search algorithm to optimise parameters of SVM
+3) implement K-fold cross validation method to assure model not biased
+4) implement evaluation methods ( Precision, Recall, Accuracy )
+5) save model with pickle
+    GA
+* implement leverage activation function from x2 to x10 
+1) train 3 Genetic Algorithms for different market types
+2) 
+    Backtesting
+
+'''
+data_path = '/Users/Dimasik007/Desktop/Programming/PyCharm/Dissertation/svm_ga_system/data/'
+
+# FX Data Layer
+
+# load csv file to df
+EU = pd.read_csv(data_path+'EURUSD_1H.csv', index_col=['Time'], usecols=['Time', 'Close', 'Volume'],
+                 parse_dates=True, infer_datetime_format=True, dayfirst=True)
+
+EUR_USD = EU.copy()
+
+# to delete rows which have 0 volume / delete weekends
+EUR_USD = EUR_USD[(EUR_USD[['Volume']] != 0).all(axis=1)]
+
+# calculate technical indicators with ta-lib
+EUR_USD['RSI'] = ta.RSI(EUR_USD.Close.values, timeperiod=14)
+EUR_USD['ROC'] = ta.ROC(EUR_USD.Close.values, timeperiod=10)
+EUR_USD['SMA'] = ta.SMA(EUR_USD.Close.values, timeperiod=30)
+EUR_USD['EMA'] = ta.EMA(EUR_USD.Close.values, timeperiod=30)
+EUR_USD['WMA'] = ta.WMA(EUR_USD.Close.values, timeperiod=30)
+EUR_USD['MACD'], EUR_USD['macdSignal'], \
+        EUR_USD['macdHist'] = ta.MACD(EUR_USD.Close.values,
+                                      fastperiod=12, slowperiod=26, signalperiod=9)
+
+# to label market type based on condition
+EUR_USD['mType'] = np.where(EUR_USD['ROC'] > 0.5, 1,
+                            (np.where(EUR_USD['ROC'] < -0.5, -1, 0)))
+
+# shift positively - move values by x values down
+EUR_USD['mType1'] = EUR_USD['mType'].shift(1).fillna(9).astype(int)
+# delete rows with value 9 in shifted market type
+EUR_USD = EUR_USD[(EUR_USD[['mType1']] != 9).all(axis=1)]
+
+# drop nan values left from TI
+EUR_USD.dropna(inplace=True)
+
+# trading rules per technical indicators
+EUR_USD['R_RSI'] = np.where(EUR_USD['RSI'] > 70, 1,
+                            (np.where(EUR_USD['RSI'] < 30, -1, 0)))
+EUR_USD['R_ROC'] =
+
+# plotting TIs and closing price on one plot
+"""
+# to plot specific time period
+# EUR_USD['2017-01-01':'2018-06-01'][['close']].plot()
+# plt.show()
+
+# Prepare plot (4 subplots on 1 column)
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1, sharex=True)
+# Remove horizontal space between axes
+fig.subplots_adjust(hspace=0)
+
+# set plot size
+fig.set_size_inches(15, 30)
+
+# plotting closing prices
+ax1.set_ylabel('EUR / USD', size=20)
+EUR_USD.Close.resample('M').mean().plot(ax=ax1, c='black')
+# plotting moving averages
+EUR_USD.sma.resample('M').mean().plot(ax=ax1, c='r', label='SMA30')
+EUR_USD.ema.resample('M').mean().plot(ax=ax1, c='g', label='EMA30')
+EUR_USD.wma.resample('M').mean().plot(ax=ax1, c='b', label='WMA30')
+# setting legend
+handles, labels = ax1.get_legend_handles_labels()
+ax1.legend(handles, labels)
+
+# plotting RSI subplot
+ax2.set_ylabel('RSI', size=20)
+EUR_USD.rsi.resample('M').mean().plot(ax=ax2, c='g', label='RSI 14')
+ax2.axhline(y=30, c='b')
+ax2.axhline(y=50, c='black')
+ax2.axhline(y=70, c='b')
+ax2.set_ylim([0, 100])
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend(handles, labels)
+
+# plotting MACD
+ax3.set_ylabel('MACD: 12, 26, 9', size=20)
+EUR_USD.macd.resample('M').mean().plot(ax=ax3, color='b', label='Macd')
+EUR_USD.macdSignal.resample('M').mean().plot(ax=ax3, color='g', label='Signal')
+EUR_USD.macdHist.resample('M').mean().plot(ax=ax3, color='r', label='Hist')
+ax3.axhline(0, lw=2, color='0')
+handles, labels = ax3.get_legend_handles_labels()
+ax3.legend(handles, labels)
+
+# plotting ROC
+ax4.set_ylabel('ROC: 10', size=20)
+ax4.set_xlabel('Year', size=25)
+EUR_USD.roc.resample('M').mean().plot(ax=ax4, color='r', label='ROC')
+handles, labels = ax4.get_legend_handles_labels()
+ax4.legend(handles, labels)
+
+# plt.show()_____________________________________________________________________________________________________
+"""
+
+# plotting market types and closing price together 
+"""
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(20, 10))
+fig.subplots_adjust(hspace=0)
+ax1.set_ylabel('EUR / USD')
+EUR_USD['2015-01-01':'2017-01-01'][['Close']].plot(ax=ax1, c='g')
+ax2.set_ylabel('Market Type')
+EUR_USD['2015-01-01':'2017-01-01'][['mType']].plot(ax=ax2)
+plt.show()
+"""
+
+
+# OPTIMISATION LAYER SVM______________________________________________________
+def rebalance(unbalanced_data):
+
+    # Separate majority and minority classes
+    data_minority = unbalanced_data[unbalanced_data.target == 0]
+    data_majority = unbalanced_data[unbalanced_data.target == 1]
+
+    # Upsample minority class
+    n_samples = len(data_majority)
+    data_minority_upsampled = resample(data_minority, replace=True, n_samples=n_samples, random_state=5)
+
+    # Combine majority class with upsampled minority class
+    data_upsampled = pd.concat([data_majority, data_minority_upsampled])
+
+    data_upsampled.sort_index(inplace=True)
+
+    # Display new class counts
+    data_upsampled.target.value_counts()
+
+    return data_upsampled
+
+
+def normalise(x):
+    # Standardize features by removing the mean and scaling to unit variance
+    scaler = StandardScaler()
+    x_norm = scaler.fit_transform(x.values)
+    x_norm = pd.DataFrame(x_norm, index=x.index, columns=x.columns)
+
+    return x_norm
+
+
+# TODO print evaluation for non binary labels
+def scores(model, X, y):
+    # print results
+    # for model in models:
+    y_pred = model.predict(X)
+    prec = precision_score(y, y_pred)
+    rec = recall_score(y, y_pred)
+    acc = accuracy_score(y, y_pred)
+    f1 = f1_score(y, y_pred)
+    print("Predictions: {}".format(y_pred[0:5]))
+    print("Precision score: {}".format(prec))
+    print("Recall score: {}".format(rec))
+    print("Accuracy Score: {0:0.2f} %".format(acc * 100))
+    print("F1 Score: {0:0.4f}".format(f1))
+
+
+def table2lags(table, max_lag, min_lag=0, separator='_'):
+    """ Given a dataframe, return a dataframe with different lags of all its columns """
+    values = []
+    for i in range(min_lag, max_lag + 1):
+        values.append(table.shift(i).copy())
+        values[-1].columns = [c + separator + str(i) for c in table.columns]
+    return pd.concat(values, axis=1)
+
+
+a = EUR_USD.copy()
+j = table2lags(a[['Close']], 99)
+print(j.head())
+j['mType'] = a['mType']
+j.dropna(inplace=True)
+
+
+# machine learning here________________________________________________________________________________________
+# preparing data for ml
+data_train = a[:'2005-12-31']  # create training df
+y = data_train['mType1'].values  # target which we predict ( .values to transform it to ndarray )
+# y = label_binarize(y, classes=[-1, 0, 1])  ######### probably not needed
+X = data_train.drop(['mType', 'mType1', 'Close', 'Volume'], axis=1)  # data price sequences type 1
+X = normalise(X)  # normalise X ( leads to x3 accuracy improvement )
+X = X.values  # change type to ndarray
+# X = data_train[['RSI', 'ROC', 'SMA', 'EMA', 'WMA', 'MACD', 'macdSignal', 'macdHist']]  # data TI sequences type 2
+
+# validation set
+data_val = EUR_USD['2015-01-01':]
+y_val = data_val['mType']  # target
+X_val = data_val[['Open', 'High', 'Low', 'Close']]  # data price sequences type 1
+# TODO don't forget to normalise the data X_val
+# X_val = data_val[['RSI', 'ROC', 'SMA', 'EMA', 'WMA', 'MACD', 'macdSignal', 'macdHist']]  # data TI sequences type 2
+
+# splitting data for training and testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+print("Training data: {} and target: {}".format(X_train.shape, y_train.shape))
+print("Training data: {} and target: {}".format(X_test.shape, y_test.shape))
+
+# adjust weights of the classes to account for unbalanced data
+class_weights = class_weight.compute_class_weight('balanced', np.unique(y_train), y_train)
+print(class_weights)
+
+# check the sum of each value
+unique, counts = np.unique(y, return_counts=True)
+dict(zip(unique, counts))
+
+# specify parameters for classifier
+svc_parameters = [{'kernel': ['poly'], 'degree': [2, 3, 4], 'C': [1, 10, 100, 1000],
+                   'gamma': [0.0001, 0.001, 0.01, 0.1, 1]},
+                  {'kernel': ['rbf'], 'C': [1, 10, 100, 1000], 'gamma': [0.0001, 0.001, 0.01, 0.1, 1]},
+                  {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}
+                  ]
+
+# do exhaustive search for best parameters to find those that max accuracy score and do K-fold with K=5
+clf = GridSearchCV(SVC(class_weight={-1: 3.74030354, 0: 0.40926285, 1: 3.45752143}),
+                   param_grid=svc_parameters, cv=5, scoring='accuracy',
+                   n_jobs=-1, refit=True, return_train_score=False, verbose=100)
+
+# fit input (X_train: price sequences / TIs) to target (y_train: 1, 0, -1)
+clf.fit(X_train, y_train)
+
+# print all the results
+clf.score(X_train, y_train)
+print("Best parameters set found on development set:")
+print()
+print(clf.best_params_)
+print()
+print("Grid scores on training set:")
+print()
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+    print("%0.3f (+/-%0.03f) for %r"
+          % (mean, std * 2, params))
+print()
+
+# print("{0:0.3f} (+/-{0:0.03f}) for {}".format(mean, std * 2, params))
+
+#
+y_pred = clf.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print('Support Vector Machine Classifier\n {}\n'.format(classification_report(y_test, y_pred,
+                                                                              target_names=['Downtrend',
+                                                                                            'Sideways',
+                                                                                            'Uptrend'])))
+print("Accuracy Score: {0:0.2f} %".format(acc * 100))
+
+print(clf.best_score_)
+
+# print scores of clf.predict ( fix non binary labels )
+scores(clf, X_test, y_test)
+
+"""
+clf = OneVsRestClassifier(SVC(kernel='rbf', C=10, gamma=0.001))
+clf.fit(X_train, y_train)
+clf.score(X_train, y_train)
+"""
+
+# clf = SVC(class_weight={-1: 3.74237288, 0: 0.40858623, 1: 3.5047619})
+
+"""
+# get best parameters for classifier
+for i in range(1):
+    grid = GridSearchCV(param_grid[i], scoring='f1').fit(X_train, y_train)
+    print(grid.best_params_)
+    model_best = grid.best_estimator_
+"""
+
+# save model to pickle file
+# saved_model = pickle.dumps(clf)
+# clf2 = pickle.loads(saved_model)
+# clf2.predict(X)
+
+"""
+with open('svc_model_prices.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+pickle_in = open('svc_model_prices.pickle', 'rb')
+clf_from_pickle = pickle.load(pickle_in)
+
+# do cross validation
+Kcv = KFold(n_splits=5)
+prediction_cv = cross_val_score(clf, X, y, cv=cv)
+
+# defining classification algorithm
+clf = SVC()
+# fit input (X_train: price sequences) to target (Y_train: 1, 0, -1)
+clf.fit(X_train, y_train)
+
+
+scores(clf, X_test, y_test)
+"""
+
+# score on validation set
+scores(model_best, X_val, y_val)
+
+count = 100
+for i in range(0, count):
+    b["Close_{}".format(str(i+1))] = b["Close"].shift(i+1)
+
+'''
+def series_to_supervised( data, n_in=1, n_out=1, dropnan=True ):
+    """
+    Frame a time series as a supervised learning dataset.
+    Arguments:
+        data: Sequence of observations as a list or NumPy array.
+        n_in: Number of lag observations as input (X).
+        n_out: Number of observations as output (y).
+        dropnan: Boolean whether or not to drop rows with NaN values.
+    Returns:
+        Pandas DataFrame of series framed for supervised learning.
+    """
+    n_vars = 1 if type( data ) is list else data.shape[ 1 ]
+    df = pd.DataFrame( data )
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range( n_in, 0, -1 ):
+        cols.append( df.shift( i ) )
+        names += [ ('var%d(t-%d)' % (j + 1, i)) for j in range( n_vars ) ]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range( 0, n_out ):
+        cols.append( df.shift( -i ) )
+        if i == 0:
+            names += [ ('var%d(t)' % (j + 1)) for j in range( n_vars ) ]
+        else:
+            names += [ ('var%d(t+%d)' % (j + 1, i)) for j in range( n_vars ) ]
+    # put it all together
+    agg = pd.concat( cols, axis=1 )
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna( inplace=True )
+    return agg
+
+
+data = series_to_supervised(EUR_USD[['RSI', 'ROC', 'SMA', 'EMA', 'WMA',
+                                     'MACD', 'macdSignal', 'macdHist']].values, 2, 2, dropnan=False)
+data.head()
+'''
